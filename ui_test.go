929cddeb8b092c87c12e272fed48924b03943d26
BarryODev <barryodev@gmail.com>
2021-05-06 19:06:59 +0100

    added more ui tests

--- ui_test.go
+++ ui_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 )
 
+const testUrlOne = "theregistry.com"
+const testUrlTwo = "google.com"
+
 func TestAppRun(t *testing.T) {
 	app := CreateStubbedApp(false)
 	ui := CreateUI(app)
@@ -51,4 +54,74 @@ func TestCreateUISetupPages(t *testing.T) {
 	assert.Equal(t, feedPage, frontPage)
 
 	assert.Equal(t, ui.flex, primitive)
+}
+
+func TestFeedsListContainsFetchingDataOnStartup(t *testing.T){
+	app, simScreen := CreateTestAppWithSimScreen(100, 100)
+	defer simScreen.Fini()
+
+	ui := CreateUI(app)
+	ui.feedList.Box.SetRect(50,50,50,50)
+	ui.feedList.Draw(simScreen)
+
+	simScreen.Show()
+
+	simCells, _, _ := simScreen.GetContents()
+
+	screenContents := make([]rune, len(simCells))
+	for i := 0; i < len(simCells); i++ {
+		screenContents[i] = simCells[i].Runes[0]
+	}
+
+	screenContentsAsString := string(screenContents)
+
+	assert.Contains(t, screenContentsAsString, "Fetching Feed Data")
+}
+
+func TestFeedsListContainsFeedNameAfterLoadingData(t *testing.T) {
+	app := CreateStubbedApp(true)
+	ui := CreateUI(app)
+
+	data := createTestData()
+
+	ui.updateInterface(data)
+
+	for _, f := range ui.app.(*StubbedApp).UpdateDraws {
+		f()
+	}
+
+	listItemOne, _ := ui.feedList.GetItemText(0)
+	assert.Equal(t, "registry", listItemOne)
+
+	listItemTwo, _ := ui.feedList.GetItemText(1)
+	assert.Equal(t, "google", listItemTwo)
+}
+
+func createTestData() *Data {
+	safeFeedData := &SafeFeedData{feedData: make(map[string]FeedDataModel)}
+	safeFeedData.SetSiteData(testUrlOne, createFakeFeedDataModel("registry"))
+	safeFeedData.SetSiteData(testUrlTwo, createFakeFeedDataModel("google"))
+
+	allFeeds := &AllFeeds{[]Feed{Feed{testUrlOne}, {testUrlTwo}}}
+	return &Data{safeFeedData: safeFeedData, allFeeds: allFeeds}
+}
+
+func createFakeFeedDataModel(name string) FeedDataModel {
+	fakeEntryOne := Entry{
+		title:   name + " fake title one",
+		url:     testUrlOne + "/one",
+		content: name + " fake content one",
+	}
+
+	fakeEntryTwo := Entry{
+		title:   name + " fake title two",
+		url:     testUrlOne + "/two",
+		content: name + " fake content two",
+	}
+
+	fakeFeedDataModelOne := FeedDataModel{
+		name:    name,
+		entries: []Entry{fakeEntryOne, fakeEntryTwo},
+	}
+	return fakeFeedDataModelOne
 }
